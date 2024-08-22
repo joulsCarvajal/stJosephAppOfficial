89006e557b07083d26d1896cabebaacdb798e781
joulsCarvajal <juliancarvajaldiaz1992@outlook.com>
2024-08-22 13:29:43 -0500

    Actualizar AndroidManifest y MainActivity

--- app/src/main/java/com/alphazetakapp/stjosephappofficial/ui/MainActivity.kt
+++ app/src/main/java/com/alphazetakapp/stjosephappofficial/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.alphazetakapp.stjosephappofficial.ui
 
+import android.content.pm.ActivityInfo
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalContext
 import com.alphazetakapp.stjosephappofficial.navigation.AppNavigation
 import com.alphazetakapp.stjosephappofficial.ui.theme.StjosephappofficialTheme
 import com.google.android.gms.ads.MobileAds
@@ -14,10 +17,20 @@ class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //Este sería el tema de arranque
         enableEdgeToEdge()
         setContent {
             StjosephappofficialTheme {
+                DisposableEffect(Unit) {
+                    val originalOrientation = requestedOrientation
+                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+                    onDispose {
+                        // Esto es opcional, pero permite volver a la orientación original si es necesario
+                        requestedOrientation = originalOrientation
+                    }
+                }
                 AppNavigation()
             }
         }
