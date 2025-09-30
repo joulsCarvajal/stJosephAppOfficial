57a2469442a23932bef87b9696956d175bf7893d
Edward Julian Carvajal Diaz <94931842+joulsCarvajal@users.noreply.github.com>
2025-09-29 22:31:49 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alphazetakapp.stjosephappofficial"
         minSdk = 22
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.15"
+        versionCode = 16
+        versionName = "1.0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
