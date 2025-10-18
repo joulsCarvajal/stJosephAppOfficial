3f80a86b5bb167670d9c7b242f21c621fb95067a
Julian Carvajal <edward.carvajal@avianca.com>
2025-10-17 22:33:57 -0500

    update version 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.alphazetakapp.stjosephappofficial"
         minSdk = 22
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0.17"
+        versionCode = 18
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
